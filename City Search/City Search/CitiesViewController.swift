86a85bd4ff983727b1995c1e3916e289ac6c5670
Olav Gausaker <olav.gausaker@itslearning.com>
2018-03-24 12:54:34 +0100

    Added CitiesViewController
    Added large font heading

---
+++ City Search/City Search/CitiesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CitiesViewController.swift
+//  City Search
+//
+//  Created by Olav Gausaker on 24/03/2018.
+//  Copyright © 2018 Olav Gausaker. All rights reserved.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
+
