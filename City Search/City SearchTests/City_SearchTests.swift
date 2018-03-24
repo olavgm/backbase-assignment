24fce08b5d6eceafdc47954306b8a4b631149741
Olav Gausaker <olav.gausaker@itslearning.com>
2018-03-24 12:38:44 +0100

    Empty project

---
+++ City Search/City SearchTests/City_SearchTests.swift
@@ -0,0 +1,36 @@
+//
+//  City_SearchTests.swift
+//  City SearchTests
+//
+//  Created by Olav Gausaker on 24/03/2018.
+//  Copyright © 2018 Olav Gausaker. All rights reserved.
+//
+
+import XCTest
+@testable import City_Search
+
+class City_SearchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
