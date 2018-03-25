a04fae1e92852476f9fad729f150c55840187086
Olav Gausaker <olav.gausaker@itslearning.com>
2018-03-25 13:16:11 +0200

    Tests for valid and invalid searches

--- City Search/City SearchTests/City_SearchTests.swift
+++ City Search/City SearchTests/City_SearchTests.swift
@@ -21,16 +21,38 @@ class City_SearchTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testValidSearches() {
+        let searches = ["Marbella", "Kiev", "Oslo", "New York", "San Francisco", "Miami", "Lviv", "Beijing"]
+
+        for search in searches {
+            CitiesController.shared.filter(search: search)
+
+            for index in CitiesController.shared.resultsIndex.first..<CitiesController.shared.resultsIndex.last {
+                let city = CitiesController.shared.items[index]
+
+                if !city.name.starts(with: search) {
+                    XCTFail("The result \"\(city.name)\" does not match the search query \"\(search)\".")
+                }
+            }
         }
+
+        XCTAssert(true, "Searches \(searches) found their respective results.")
+    }
+
+    func testInvalidSearches() {
+        let searches = ["asdf", "Kieva", "Oslu", "New Yorks", "Sanfrancisco", "Miamii", "Lbiv", "Beiging"]
+        
+        for search in searches {
+            CitiesController.shared.filter(search: search)
+            
+            for index in CitiesController.shared.resultsIndex.first..<CitiesController.shared.resultsIndex.last {
+                let city = CitiesController.shared.items[index]
+
+                XCTFail("The search \"\(search)\" wrongfully found a match on \"\(city.name)\".")
+            }
+        }
+        
+        XCTAssert(true)
     }
     
 }
